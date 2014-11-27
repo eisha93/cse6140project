395cc3060b3a67968ac7f81a06329759e411a61c
Eisha <eisha.nathan@gmail.com>
2014-11-27 15:07:48 -0500

    comments

--- hillClimbing.py
+++ hillClimbing.py
@@ -22,28 +22,29 @@ def hillclimb(G, all_combs, opt_sol):
 		#print iterations
 		#print "huh2 " + str(curr_soln)
 		
+		#at each step of "climbing the hill" the algorithm looks in its "neighborhood" of the current solution
+		#find_next_soln returns the 'best' soln in the current soln's neighborhood
 		temp_cost, next_soln = find_next_soln(curr_soln, G, all_combs)
 		#print temp_cost
 		#print next_soln
 
-		if temp_cost <= (q*opt_sol) + opt_sol: 
-			print "LOLOLOLOL"
+		#ignore for now -- for trace files
+		#if temp_cost <= (q*opt_sol) + opt_sol: 
+			#print "LOLOLOLOL"
 
-		#print "huh3 " + str(curr_soln) + " nextsoln " + str(next_soln)
-		#temp_cost = bb.find_cost(next_soln)
 		if temp_cost >= curr_cost:
-			#print "woo" + str(iterations)
 			return curr_cost,curr_soln #meaning we have reached the "peak"
+
 		curr_cost = temp_cost
-		#print curr_cost
 		curr_soln = next_soln
 
 		iterations += 1
 	#print str(curr_soln)
 	return curr_cost,curr_soln
 
-
+#given a current solution, returns the best solution in its neighborhood
 def find_next_soln(curr_soln, G, all_combs):
+	#find_successors uses 2opt exchange to return the neighborhood of all possible solutions obtained by reversing some part of the current solutions route
 	successors = find_successors(curr_soln, G, all_combs)
 	best_soln = None
 	best_cost = float("inf")
@@ -69,20 +70,18 @@ def all_combinations(G):
 				all_combos.append((i,j))
 	return all_combos
 
+#given a current solution, returns a list of the neighborhood of the current solution
+#neighborhood is defined as all possible variations of a route where a variation is obtained by reversing part of a route
 def find_successors(curr_soln, G, all_combs):
-	#print "calling find successors"
-	#given a current solution find its "neighbors"?
+	#given a current solution find its "neighbors"
 	#use 2-OPT to find all possible variations of the curr_soln --> find all possible variations of swapping
 	successors = []
 
 	n = len(G.nodes()) - 1
-	#print str(bb.find_cost(curr_soln)) + "AOGHOSG"
 	
 	#print all_combs
 	for (i,j) in all_combs:
 		if i<j:
-			#temp = copy.deepcopy(curr_soln)
-			#print "HAI" + str(curr_soln) #+ " " + str(bb.find_cost(curr_soln,G))
 			new_route = []
 			new_route[0:i] = copy.deepcopy(curr_soln[0:i])
 			new_route[i:j] = list(reversed(curr_soln[i:j]))
@@ -90,7 +89,9 @@ def find_successors(curr_soln, G, all_combs):
 			successors.append(new_route)
 	return successors
 
-
+#hill climbing with restart 
+#This method calls hillclimb (above) "num_iter" times -- each time the method returns a locally optimal solution. Calling hillclimb multiple times allows us to not only find a local optimum
+#but greatly increases our chances of finding the globally optimal solution. 
 def hctour(G, trfilename, opt_sol):
 	trfile = open(trfilename, 'w')
 	#start_time = time.time()
@@ -100,12 +101,16 @@ def hctour(G, trfilename, opt_sol):
 	best_soln = None
 	best_cost = float("inf")
 
+	#gets all the possible pairs of nodes (1,2),(1,3),...etc -- used in 2-opt exchange
 	all_combs = all_combinations(G)
 
+	#calls hillclimb multiple times to hopefully find optimal solution
 	while iterations < num_iter:
 		#print "hi" + str(iterations)
 		new_cost,new_soln = hillclimb(G, all_combs, opt_sol)
 		print new_cost
+
+		#if it finds a better solution than previous solution, reset best solution
 		if best_soln is None:
 			best_soln = new_soln
 			best_cost = new_cost
@@ -116,6 +121,7 @@ def hctour(G, trfilename, opt_sol):
 			#trfile.write(str(time.time() - start_time) + ", " + str(best_cost)+"\n")
 		iterations += 1
 
+	#makes it a cycle
 	best_soln.append(best_soln[0])
 
 	return best_soln, best_cost
