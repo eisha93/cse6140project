435799c3ff200a3ddc91a302f88e49eb14c4fc41
Eisha <eisha.nathan@gmail.com>
2014-12-01 16:03:04 -0500

    sdof

--- collect_data.py
+++ collect_data.py
@@ -5,10 +5,10 @@ import hillClimbing as hc
 rt = run_tsp.RunTSP()
 G, opt_sol = rt.create_graph('ch150.tsp')
 
-qs = [.1,.15,.2,.25]
+qs = [.1,.2,.3]
 algorithm = 'hill_climbing'
 
-times = [120, 180, 240, 300, 360, 420]
+times = [600, 1200, 1800]
 filename = 'ch150.tsp'
 random_seed = 1
 
@@ -17,13 +17,41 @@ alldata = open('ch150data.txt', 'w')
 
 next = 1
 
+seeds = [1,34,54,12,65,73,53,74,255,67]
+
 for q in qs:
 	for cutoff_time in times:
 		count = 0
 		for i in range(10):
-			trfilename = filename[:(len(filename)-4)] + "_" + algorithm + "_" + str(cutoff_time) + "_" + str(random_seed) + ".trace"
+			trfilename = filename[:(len(filename)-4)] + "_" + algorithm + "_" + str(cutoff_time) + "_" + str(i+1) + ".trace"
 			start_hc = time.time()
-			hc_tour, hc_cost, q_yes_no = hc.hctour(G, trfilename, opt_sol, cutoff_time, q)
+			hc_tour, hc_cost, q_yes_no = hc.hctour(G, trfilename, opt_sol, cutoff_time, q, seeds[i])
+			datafile.write(str(q) + "," + str(cutoff_time) + "," + str(i+1) + "," + str(q_yes_no) + "\n")
+			if q_yes_no=='yes':
+				count += 1
+		alldata.write(str(q)+","+str(cutoff_time)+","+str(count)+"\n")
+
+G, opt_sol = rt.create_graph('ch150.tsp')
+
+qs = [.1,.2,.3]
+algorithm = 'hill_climbing'
+
+times = [600, 1200, 1800]
+filename = 'gr202.tsp'
+random_seed = 1
+
+datafile = open('gr202rawdata.txt', 'w')
+alldata = open('gr202data.txt', 'w')
+
+next = 1
+
+for q in qs:
+	for cutoff_time in times:
+		count = 0
+		for i in range(10):
+			trfilename = filename[:(len(filename)-4)] + "_" + algorithm + "_" + str(cutoff_time) + "_" + str(i+1) + ".trace"
+			start_hc = time.time()
+			hc_tour, hc_cost, q_yes_no = hc.hctour(G, trfilename, opt_sol, cutoff_time, q, seeds[i])
 			datafile.write(str(q) + "," + str(cutoff_time) + "," + str(i+1) + "," + str(q_yes_no) + "\n")
 			if q_yes_no=='yes':
 				count += 1
--- runtests.sh
+++ runtests.sh
@@ -1,31 +1,31 @@
 
-echo "hill climbing\n"
+echo "hill climbing"
 python run_tsp.py burma14.tsp 3600 hill_climbing 12
-echo "\n"
+echo "--"
 python run_tsp.py ulysses16.tsp 3600 hill_climbing 154
-echo "\n"
+echo "--"
 python run_tsp.py berlin52.tsp 3600 hill_climbing 83
-echo "\n"
+echo "--"
 python run_tsp.py kroA100.tsp 3600 hill_climbing 44
-echo "\n"
+echo "--"
 python run_tsp.py ch150.tsp 3600 hill_climbing 3333
-echo "\n"
+echo "--"
 python run_tsp.py gr202.tsp 3600 hill_climbing 12345
-echo "\n"
-echo "\n"
+echo "--"
+echo "--"
 
-echo "branch and bound\n"
+echo "branch and bound"
 
 python run_tsp.py burma14.tsp 3600 branch_and_bound 12
-echo "\n"
+echo "--"
 python run_tsp.py ulysses16.tsp 3600 branch_and_bound 154
-echo "\n"
+echo "--"
 python run_tsp.py berlin52.tsp 3600 branch_and_bound 83
-echo "\n"
+echo "--"
 python run_tsp.py kroA100.tsp 3600 branch_and_bound 44
-echo "\n"
+echo "--"
 python run_tsp.py ch150.tsp 3600 branch_and_bound 3333
-echo "\n"
+echo "--"
 python run_tsp.py gr202.tsp 3600 branch_and_bound 12345
-echo "\n"
+echo "--"
 
