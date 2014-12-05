f268acb8d01a623b76462812c30b177a4183277e
lanseafood <l.mao.ftwin@gmail.com>
2014-12-05 12:05:16 -0500

    Commented SimAnneal and MST approx

--- simAnneal.py
+++ simAnneal.py
@@ -1,3 +1,5 @@
+#SIMULATED ANNEALING LOCAL SEARCH ALGORITHM
+"""This file performs a SIMULATED ANNEALING iterated local search on the given input graph."""
 import numpy as np
 import branchAndBound as bb
 import nearestNeighbor as nn
@@ -8,6 +10,7 @@ import random
 import math
 
 def simAnneal(G,trfilename, opt_sol, cutoff_time, seed):
+	"""Returns the best solution and best cost on a input graph for a specified starting and ending temperature with alpha cooling factor set to .85. """
 	random.seed(seed)
 	trfile = open(trfilename, 'w')
 	start_time = time.time()
@@ -15,7 +18,7 @@ def simAnneal(G,trfilename, opt_sol, cutoff_time, seed):
 	t = 1.0e+10 #lower
 	t_end = 0.001
 	curr_soln, e = nn.nntour(G, 'fake')
-	#print curr_soln
+
 	#initializing first solution
 	best_soln = curr_soln
 	best_cost = e
@@ -23,7 +26,6 @@ def simAnneal(G,trfilename, opt_sol, cutoff_time, seed):
 	trfile.write(str(time.time() - start_time) + ", " + str(best_cost)+"\n")
 
 	all_combs = all_node_combos(G)
-	#print best_cost
 	while t > t_end:
 		if (time.time()-start_time) >= cutoff_time:
 			print 'ran out of time'
@@ -35,17 +37,18 @@ def simAnneal(G,trfilename, opt_sol, cutoff_time, seed):
 				best_soln = curr_soln
 				e = n_cost
 				best_cost = e
-				#print best_cost
-				if best_cost < trace_cost:
+				if best_cost < trace_cost: # updates the trace file with the next best cost
 					trace_cost = best_cost
 					trfile.write(str(time.time() - start_time) + ", " + str(best_cost)+"\n")
-				#print best_cost
 			t = temp(t,alpha)
 
 	best_soln.append(best_soln[0])
 	return best_soln, best_cost
 
+
+#finds all combinations of each edge and two vertices.
 def all_node_combos(G):
+	"""Returns all possible pairings of nodes. Used in 2-opt exchange."""
 	all_combos = []
 	n = len(G.nodes()) - 1
 
@@ -53,37 +56,32 @@ def all_node_combos(G):
 		for j in range(1,n):
 			if i<j:
 				all_combos.append((i,j))
-	#print all_combos
 	return all_combos
 
 def find_some_neighbor(curr_soln, G, all_combs):
+	"""Finds a random neighbor from the best k neighbors in the entire sorted neighborhood from best cost to worst cost."""
 	neighbors = find_neighbors(curr_soln, G, all_combs)
 	nays = {}
 	for n in neighbors:
 		rank = bb.find_cost(n, G)
 		if rank not in nays:
-			#print'havent seen before'
 			nays[rank] = n
 	keylist = nays.keys()
+	#defines k to be the top 5 percent of the current solution's neighbors in its neighborhood.
 	k = int(round(.03*len(keylist),0))
 	keylist = sorted(keylist)
-	# print keylist
-	#nays = sorted(nays, key=nays.get)
-	#print nays
-	# print keylist[:k]
-	random_n = random.choice(keylist[:k])
+	random_n = random.choice(keylist[:k]) #randomly chooses a neighbor from the top 5 percent
 	r_n = nays[random_n]
 	cost_n = random_n
-	#print cost_n
 	return r_n, cost_n
 
 def find_neighbors(curr_soln, G, all_combs):
-	#print all_combs
 	#given a current solution find its "neighbors"
 	#use 2-OPT to find all possible variations of the curr_soln --> find all possible variations of swapping
+	"""Given a current solution, returns a list of the neighborhood of the current solution, where a neighborhood 
+	is defined as all possible variations of a route (variation is obtained by reversing part of a route using 2-opt exchange)."""
 	successors = []
 	n = len(G.nodes()) - 1
-	#print all_combs
 	for (i,j) in all_combs:
 		if i<j:
 			new_route = []
@@ -94,13 +92,11 @@ def find_neighbors(curr_soln, G, all_combs):
 	return successors
 
 def temp(start_temp,alpha):
+	"""Updates the Temperature by mulitplying itself by alpha when called"""
 	T=start_temp*alpha
 	return T
 
 def P(prev_score, next_score, temp):
-	# if next_score < prev_score:
-	# 	return 1.0
-	# else:
-	#print math.exp( -abs(next_score-prev_score)/temp)
+	"""Reuturns a probability of accepting a worse solution based off the current temperature value"""
 	return math.exp( -abs(next_score-prev_score)/temp)
 
