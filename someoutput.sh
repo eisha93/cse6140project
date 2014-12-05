1e730f55ce12b0252257447a217a870987622659
Eisha <eisha.nathan@gmail.com>
2014-12-05 13:41:14 -0500

    everything

---
+++ someoutput.sh
@@ -0,0 +1,15 @@
+
+#nn
+python run_tsp.py burma14.tsp 600 mst_approx 22
+python run_tsp.py ulysses16.tsp 600 mst_approx 22
+python run_tsp.py berlin52.tsp 600 mst_approx 22
+python run_tsp.py kroA100.tsp 600 mst_approx 22
+python run_tsp.py ch150.tsp 600 mst_approx 22
+python run_tsp.py gr202.tsp 600 mst_approx 22
+
+python run_tsp.py burma14.tsp 600 nearest_neighbor 22
+python run_tsp.py ulysses16.tsp 600 nearest_neighbor 22
+python run_tsp.py berlin52.tsp 600 nearest_neighbor 22
+python run_tsp.py kroA100.tsp 600 nearest_neighbor 22
+python run_tsp.py ch150.tsp 600 nearest_neighbor 22
+
